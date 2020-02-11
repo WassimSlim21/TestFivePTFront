9377d45327d8de276f3a7dc6199fc7cc9ed92080
WassimSlim21 <wassimslim18@gmail.com>
2020-02-11 14:18:39 +0100

    add admin layout lazy loading module

---
+++ src/app/layout/admin-layout/admin-layout-routing.module.ts
@@ -0,0 +1,47 @@
+import { Routes } from '@angular/router';
+export const AdminLayoutRoutes: Routes = [
+    // {
+    //   path: '',
+    //   children: [ {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // }]}, {
+    // path: '',
+    // children: [ {
+    //   path: 'userprofile',
+    //   component: UserProfileComponent
+    // }]
+    // }, {
+    //   path: '',
+    //   children: [ {
+    //     path: 'icons',
+    //     component: IconsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'notifications',
+    //         component: NotificationsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'maps',
+    //         component: MapsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'typography',
+    //         component: TypographyComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'upgrade',
+    //         component: UpgradeComponent
+    //     }]
+    // }
+  ];
+
+export class AdminRoutingModule { }
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+
+let logo = 'assets/img/kpeiz_logo.png';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/users', title: 'Kpeiz Users',  icon:'person', class: '' },
+ ];
+
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
 
 }
