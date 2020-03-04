2e227b0cead613fd68edaf63ef1476a2f9b14904
WassimSlim21 <wassimslim18@gmail.com>
2020-03-04 16:19:27 +0100

    spinner'

--- src/app/pages/tags/tags.component.ts
+++ src/app/pages/tags/tags.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+
+import { TagDetailsComponent } from 'src/app/popup/tag-details/tag-details.component';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {SelectionModel} from '@angular/cdk/collections';
 import { Tag } from 'src/app/core/models/tag';
 import { ApiService } from 'src/app/core/service/api.service';
 import { Router } from '@angular/router';
-import { MatDialog, PageEvent, MatChipInputEvent } from '@angular/material';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import * as moment from 'moment';
+import { PageEvent, MatChipInputEvent, MatSnackBar } from '@angular/material';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
-
-
-
+import { ConfirmDialogModel, ComfirmDialogComponent } from 'src/app/popup/comfirm-dialog/comfirm-dialog.component';
+import { UserDetailsComponent } from 'src/app/popup/user-details/user-details.component';
 export interface PeriodicElement {
   name: string;
   position: number;
   weight: number;
   symbol: string;
 }
-
-
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
@@ -31,11 +31,9 @@ export class TagsComponent implements OnInit {
   removable = true;
   addOnBlur = true;
   tr = true ;
+  isLoading = true;
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
-
-
   filterForm: FormGroup;
-
   displayedColumns: string[] = ['select', 'name', 'synonyms', 'type', 'updated_at', 'social_accounts', 'star' ];
   dataSource = new MatTableDataSource<Tag>();
   selection = new SelectionModel<Tag>(true, []);
@@ -47,7 +45,6 @@ export class TagsComponent implements OnInit {
   pageSizeOptions: number[] = [5, 10];
   moment = moment;
   tags: any[] = [];
-
   tagTypes = [{
     value: '0',
     name: 'Sector',
@@ -58,14 +55,13 @@ export class TagsComponent implements OnInit {
     value: '2',
     name: 'Provider',
   }];
-
-
-  constructor(private apiService: ApiService, private router: Router, public dialog: MatDialog, private fb: FormBuilder) { }
-
+  result: any;
+  constructor(private apiService: ApiService, private router: Router, public dialog: MatDialog, private fb: FormBuilder,
+              private snackBar: MatSnackBar,
+              public dialogRef: MatDialogRef<ComfirmDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any, ) { }
   ngOnInit() {
     this.getTags(1);
-
-
     this.filterForm = this.fb.group({
       name: new FormControl(),
       last_login: new FormControl(),
@@ -76,31 +72,30 @@ export class TagsComponent implements OnInit {
       pack: new FormControl(),
       status: new FormControl(),
     });
-
-
   }
-
-
-
-  getTags(page) {
-
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TagDetailsComponent, {
+      width: '250px'    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+getTags(page) {
     this.apiService.apiGetAll('/tag?pageNo=' + page + '&size=' + this.pageSize).subscribe(
       (response: any) => {
         if (response) {
+          this.isLoading = false;
           response.tags.forEach(element => {
             if (element.synonyms) {
               element.synonyms = element.synonyms.split(',');
                } else {
                 element.synonyms = [];
                }
-
               });
           this.length = response.total;
           this.pageIndex = response.pageIndex;
           this.tags = response.tags;
-
           this.dataSource = new MatTableDataSource<Tag>(this.tags);
-
           console.log('tags', response);
         }
       },
@@ -108,15 +103,6 @@ export class TagsComponent implements OnInit {
       console.log(error);
     });
   }
-
-
-
-
-
-
-
-
-
   onPaginateChange(event?: PageEvent) {
     this.pageSize = event.pageSize;
     if (event.pageIndex < 1) {
@@ -124,14 +110,11 @@ export class TagsComponent implements OnInit {
     }
     this.getTags(event.pageIndex);
   }
-
-
   setPageSizeOptions(setPageSizeOptionsInput: string) {
     if (setPageSizeOptionsInput) {
       this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
     }
   }
-
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -144,7 +127,6 @@ export class TagsComponent implements OnInit {
         this.selection.clear() :
         this.dataSource.data.forEach(row => this.selection.select(row));
   }
-
   /** The label for the checkbox on the passed row */
   checkboxLabel(row?: PeriodicElement): string {
     if (!row) {
@@ -152,33 +134,45 @@ export class TagsComponent implements OnInit {
     }
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
   }
-
-
-
   /* Synonyms Ships input methods */
-
   add(event: MatChipInputEvent, element: any): void {
     const input = event.input;
     const value = event.value;
-
     // Add our fruit
     if ((value || '').trim()) {
       element.synonyms.push(value.trim());
     }
-
     // Reset the input value
     if (input) {
       input.value = '';
     }
   }
-
   remove(element: any, synonym: any): void {
     const index = element.synonyms.indexOf(synonym);
-
     if (index >= 0) {
      element.synonyms.splice(index, 1);
     }
   }
+  comfirmDialog(tag: any): void {
+    const message = `Are you sure you want to do this?`;
+    const dialogData = new ConfirmDialogModel('Confirm Action', message);
+    const dialogRef = this.dialog.open(ComfirmDialogComponent, {
+      maxWidth: '400px',
+      data: dialogData
+    });
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      this.result = dialogResult;
+      const index = this.tags.indexOf(tag);
+      if ( this.result === true) {
+        this.apiService.apiDelete(`/tag/${tag._id}`).subscribe(
+          (response: any) => {
+            console.log('delete' + response);
+            this.snackBar.open(JSON.stringify(response.message));
+            this.getTags(this.pageIndex);
+          }
+      );
+        this.dialogRef.close();
+    }
+    });
+  }
 }
-
-
