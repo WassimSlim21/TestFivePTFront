e538b95d4172ccff894afc49d911cfe2667f7ef2
WassimSlim21 <wassimslim18@gmail.com>
2020-04-10 22:16:20 +0200

    update pack

--- src/app/popup/update-pack/update-pack.component.ts
+++ src/app/popup/update-pack/update-pack.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/service/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-update-pack',
@@ -7,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatePackComponent implements OnInit {
 
-  constructor() { }
+  updatePackForm: FormGroup;
+  constructor(private apiService: ApiService, private snackBar: MatSnackBar) {
+    this.updatePackForm = new FormGroup({
+      pack_name: new FormControl('', Validators.required),
+      socialAccounts: new FormControl(0, [, Validators.required]),
+      users: new FormControl(0, [Validators.required]),
+      benchmarks: new FormControl(0, [, Validators.required]),
+      annualSubscription: new FormControl(0, [, Validators.required]),
+      maxFansNumber: new FormControl(0, [, Validators.required]),
+      dataHistoryYears: new FormControl(0, [, Validators.required]),
+      dashboard: new FormControl(false, [, Validators.required]),
+      audienceInsights: new FormControl(false, [, Validators.required]),
+      trendengagementreports: new FormControl(false, [, Validators.required]),
+      customBenchmark: new FormControl(false, [, Validators.required]),
+      nationalReports: new FormControl(false, [, Validators.required]),
+      competitorContentCuration: new FormControl(false, [, Validators.required]),
+      emailReports: new FormControl(false, [, Validators.required]),
+      brandingDesign: new FormControl(false, [, Validators.required]),
+      mouthlySubcription: new FormControl(0, [, Validators.required]),
+      addPage: new FormControl(0, [, Validators.required]),
+      addUser: new FormControl(0, [, Validators.required]),
+      addBenchmark: new FormControl(0, [, Validators.required]),
 
+    });
+}
   ngOnInit() {
   }
 
