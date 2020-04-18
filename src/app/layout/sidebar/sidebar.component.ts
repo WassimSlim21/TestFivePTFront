0283e52836e801252f6fba1be30aa19fd5498a0f
WassimSlim21 <wassimslim18@gmail.com>
2020-04-18 23:34:53 +0200

    fix side bar

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -5,27 +5,30 @@ import { EditProfilepopupComponent } from 'src/app/popup/editprofile/edit-profil
 import { Router } from '@angular/router';
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
-    children: any;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+  children: any;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/users', title: 'Kpeiz Users',  icon: 'person', class: '', children : []},
-    { path: '/companys', title: 'Companies',  icon: 'apartment', class: '', children : []},
-    { path: '/tags', title: 'Tags',  icon: 'local_offer', class: '', children : []},
-    { path: '/social-accounts', title: 'Brand Insight',  icon: 'people_alt', class: '', children : []},
-    { path: '/benchmarks', title: 'Market Insight',  icon: 'ballot', class: '', children : []},
-    { path: '/packs', title: 'Packs',  icon: 'all_inbox', class: '', children : []},
-    { path: '/markcom', title: 'Marketing et Commerciale',  icon: 'web', class: '', children : [
-      { path: '/files', title: 'Files',  icon: 'attach_file', class: ''},
-      { path: '/bug', title: 'Bugs',  icon: 'bug_report', class: ''},
-      { path: '/calendar', title: 'Calendar',  icon: 'calendar_today', class: ''},
+  { path: '/users', title: 'Kpeiz Users', icon: 'person', class: '', children: [] },
+  { path: '/companys', title: 'Companies', icon: 'apartment', class: '', children: [] },
+  { path: '/tags', title: 'Tags', icon: 'local_offer', class: '', children: [] },
+  { path: '/social-accounts', title: 'Brand Insight', icon: 'people_alt', class: '', children: [] },
+  { path: '/benchmarks', title: 'Market Insight', icon: 'ballot', class: '', children: [] },
+  { path: '/packs', title: 'Packs', icon: 'all_inbox', class: '', children: [] }
+];
+export const ROUTESMarket: RouteInfo[] = [
+  {
+    path: '/markcom', title: 'Marketing et Commerciale', icon: 'web', class: '', children: [
+      { path: '/files', title: 'Files', icon: 'attach_file', class: '' },
+      { path: '/bug', title: 'Bugs', icon: 'bug_report', class: '' },
+      { path: '/calendar', title: 'Calendar', icon: 'calendar_today', class: '' },
 
-    ]},
- ];
-
+    ]
+  },
+];
 
 @Component({
   selector: 'app-sidebar',
@@ -34,21 +37,24 @@ export const ROUTES: RouteInfo[] = [
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
+  marketmenuItems: any[];
+
   isExpanded = false;
   element: HTMLElement;
   constructor(public dialog: MatDialog,
-              private authService: ApiService,
-              private router: Router
-              ) { }
+    private authService: ApiService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.marketmenuItems = ROUTESMarket.filter(marketmenuItems => marketmenuItems);
   }
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   }
   sidebarClose() {
     const body = document.getElementsByTagName('body')[0];
