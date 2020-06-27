3431c6b2e14f72bccb42116e23f57f2906e8ca38
fourat1233 <archer24183@gmail.com>
2020-06-27 16:21:05 +0100

    njareb fi faza arja3 lena if tr df

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,6 +1,8 @@
 import { OnInit } from '@angular/core';
-import { Component } from '@angular/core';
 import { schema } from './schema.value';
+import { Component, ViewChild } from '@angular/core';
+import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
+
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
@@ -8,11 +10,35 @@ import { schema } from './schema.value';
 })
 export class ConfigComponent implements OnInit {
 
+  @ViewChild(JsonEditorComponent, { static: true }) editor: JsonEditorComponent;
+
+  options = new JsonEditorOptions();
+  data = {
+    products: [{
+      name: 'car',
+      product: [{
+        name: 'honda',
+        model: [
+          { id: 'civic', name: 'civic' },
+          { id: 'accord', name: 'accord' },
+          { id: 'crv', name: 'crv' },
+          { id: 'pilot', name: 'pilot' },
+          { id: 'odyssey', name: 'odyssey' }
+        ]
+      }]
+    }]
+  };
+
+  constructor() {
+    this.options.mode = 'code';
+    this.options.modes = ['code', 'text', 'tree', 'view'];
+    this.options.schema = schema;
+    this.options.statusBar = false;
+    this.options.onChange = () => console.log(this.editor.get());
+  }
   ngOnInit(): void {
   }
-  constructor() {
 
-  }
 
 
 
